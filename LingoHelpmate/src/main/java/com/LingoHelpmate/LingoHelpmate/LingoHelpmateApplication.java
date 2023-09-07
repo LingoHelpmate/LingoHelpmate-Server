04fb394815de987b5491a0d7125e092e9e45be6c
Jimin Kim <0305jimin@gmail.com>
2023-09-07 23:08:25 +0900

    Chore: creating spring boot project file

---
+++ LingoHelpmate/src/main/java/com/LingoHelpmate/LingoHelpmate/LingoHelpmateApplication.java
@@ -0,0 +1,13 @@
+package com.LingoHelpmate.LingoHelpmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingoHelpmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingoHelpmateApplication.class, args);
+	}
+
+}
---
+++ LingoHelpmate/src/test/java/com/LingoHelpmate/LingoHelpmate/LingoHelpmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LingoHelpmate.LingoHelpmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingoHelpmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
