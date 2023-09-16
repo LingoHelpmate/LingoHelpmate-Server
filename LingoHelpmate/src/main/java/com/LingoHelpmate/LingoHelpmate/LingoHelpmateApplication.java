8139fa8a31ab2d1883817385972829903a3e4ac6
Jimin Kim <0305jimin@gmail.com>
2023-09-16 23:20:31 +0900

    Chore: enabling project to run without DB cofig

--- LingoHelpmate/src/main/java/com/LingoHelpmate/LingoHelpmate/LingoHelpmateApplication.java
+++ LingoHelpmate/src/main/java/com/LingoHelpmate/LingoHelpmate/LingoHelpmateApplication.java
@@ -2,8 +2,9 @@ package com.LingoHelpmate.LingoHelpmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class LingoHelpmateApplication {
 
 	public static void main(String[] args) {
